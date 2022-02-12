799e5ad172c48c270948f950f129c17fa50316c0
triptaSahni <86483424+triptaSahni@users.noreply.github.com>
2022-02-12 10:56:45 +0530

    Create Parenthesis Checker.java

---
+++ Stacks/Parenthesis Checker.java
@@ -0,0 +1,95 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        //Reading total number of testcases
+        int t= sc.nextInt();
+        
+        while(t-- >0)
+        {
+            //reading the string
+            String st = sc.next();
+            
+            //calling ispar method of Paranthesis class 
+            //and printing "balanced" if it returns true
+            //else printing "not balanced"
+            if(new Solution().ispar(st) == true)
+                System.out.println("balanced");
+            else
+                System.out.println("not balanced");
+        
+        }
+    }
+}// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String x)
+    {
+        // add your code here
+       
+        
+        Stack<Character> stk=new Stack<Character>();
+        for(int i=0;i<x.length();i++)
+        {
+            char c=x.charAt(i);
+            if(c=='{'||c=='('||c=='[')
+            stk.push(c);
+            else if(c=='}')
+            {
+                if(!stk.isEmpty())
+               { 
+                   char a=stk.pop();
+                if(a!='{')
+                return false;
+                   
+               }
+               else if(stk.isEmpty())
+               {
+                   return false;
+               }
+            }
+            else if(c==')')
+            {
+                if(!stk.isEmpty())
+                {
+               char a=stk.pop();
+                if(a!='(')
+                return false;
+                    
+                }
+                 else if(stk.isEmpty())
+               {
+                   return false;
+               }
+            }
+            else if(c==']')
+            {
+                if(!stk.isEmpty())
+               { char a=stk.pop();
+                if(a!='[')
+                return false;}
+            
+             else if(stk.isEmpty())
+               {
+                   return false;
+               }
+        }
+        }
+        if(stk.isEmpty()==false)
+        return false;
+      
+        return true;
+    }
+}
