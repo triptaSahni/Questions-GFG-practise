52e477cac32ac3b09828fdd46464f5dd96076e9d
Tripta Sahni <86483424+triptaSahni@users.noreply.github.com>
2022-02-21 01:28:47 +0530

    Create MinimumSwaps and K together.java

---
+++ Arrays/MinimumSwaps and K together.java
@@ -0,0 +1,81 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		     
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    int K = Integer.parseInt(br.readLine());
+		    
+		    Complete obj = new Complete();
+		    int ans = obj.minSwap(arr, sizeOfArray, K);
+		    System.out.println(ans);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int minSwap (int arr[], int n, int k) {
+        //Complete the function
+        int c=0,f=Integer.MAX_VALUE,unFav=0;
+        for(int i=0;i<n;i++)
+        {
+        if(arr[i]<=k)
+        c++;
+        }
+        if(c==0)
+        return 0;
+        else
+        {
+           for(int i=0;i<=n-c;i++)
+           {
+             for(int j=i;j<i+c;j++)
+             {
+               if(arr[j]>k) 
+               unFav++;
+             }
+              f=Math.min(unFav,f);
+              unFav=0;
+           }
+        }
+        return f;
+    }
+    
+    
+}
