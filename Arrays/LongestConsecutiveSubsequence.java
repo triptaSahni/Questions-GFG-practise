742cc32e058b1c56d184591c39c939e5734e6dd7
Tripta Sahni <86483424+triptaSahni@users.noreply.github.com>
2022-02-20 23:15:01 +0530

    Create LongestConsecutiveSubsequence.java

---
+++ Arrays/LongestConsecutiveSubsequence.java
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.math.*;
+import java.util.*;
+import java.io.*;
+
+class Driverclass
+{
+    // Driver Code
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+    }
+    
+	public static void main(String args[])
+	{
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+		int t = sc.nextInt();
+		
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			
+			for(int i=0; i<n; i++)
+				a[i] = sc.nextInt();
+		    out.println(new Solution().findLongestConseqSubseq(a, n));
+		    t--;
+		}
+		out.flush();
+	}
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{   
+    // arr[] : the input array
+    // N : size of the array arr[]
+    
+    //Function to return length of longest subsequence of consecutive integers.
+	static int findLongestConseqSubseq(int arr[], int N)
+	{
+	   // add your code here
+	   
+	   
+	    Arrays.sort(arr);
+	    int c=1,c1=1;
+      for(int i=0;i<N-1;i++)
+      {
+          if(arr[i]+1==arr[i+1])
+        {  
+            c++;
+            if(c>c1)
+            c1=c;
+            
+        }
+          else if(arr[i]==arr[i+1])
+          continue;
+          
+          else
+         { 
+        c=1;
+         }
+      }
+     return c1;
+	}
+}
